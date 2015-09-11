ceb7b2b35d39ae4d90011a7631cef2a9ccc515b4
RichardBake <richard.bake@bluegrasscoms.com>
2015-09-11 17:17:42 +0100

    Initial

---
+++ TuneCity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TuneCity.Startup))]
+namespace TuneCity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
